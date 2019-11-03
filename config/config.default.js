dc2f62417956401a0864c6a40413ac7cf704c2b7
dujiahao <610153956@qq.com>
2019-11-03 13:55:14 +0800

    config

---
+++ config/config.default.js
@@ -0,0 +1,46 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+const path = require('path');
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1572503306377_6669';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+    baiduOcr: {
+      APP_ID: "",
+      API_KEY: "",
+      SECRET_KEY: ""
+    }
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+    //按天切割日志
+    logrotator: {
+      filesRotateBySize: [
+        path.join(appInfo.root, 'logs', appInfo.name, 'egg-web.log'),
+      ],
+      maxFileSize: 2 * 1024 * 1024 * 1024,
+    },
+  };
+};
+
+exports.multipart = {
+  mode: 'file',
+};
